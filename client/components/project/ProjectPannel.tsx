b05abf66842ecfcd7d87b57975549efa06f0c070
isaac-3 <isaacgc0596@gmail.com>
2021-07-06 18:09:12 -0500

    add/style pannel submenu

--- client/components/project/ProjectPannel.tsx
+++ client/components/project/ProjectPannel.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListCard from './ListCard';
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DefocusWrapper from '../util/DefocusWrapper';
+import consts from '../../lib/data';
 
 interface Props {
   name: string;
 }
 
 const ProjectPannel = ({ name }: Props) => {
+  const [displayMenu, setDisplayMenu] = useState(false);
 
   const dummyData = [
     "List-Item1",
@@ -22,14 +26,43 @@ const ProjectPannel = ({ name }: Props) => {
     "List-Item10",
   ];
 
+  const handleOption = (option: string) => {
+    switch (option) {
+      case "Add Card": {
+        console.log(option);
+        break;
+      }
+      case "Rename Column": {
+        console.log(option);
+        break;
+      }
+      case "Delete Column": {
+        console.log(option);
+        break;
+      }
+    }
+  };
+
   return (
     <div className="project-pannel">
       <div className="project-pannel-title">
         <p>{name}</p>
         <MoreHorizRoundedIcon
-          onClick={() => console.log(name)}
           className="initial-icon"
+          onClick={() => setDisplayMenu(!displayMenu)}
         />
+        {displayMenu && (
+          <DefocusWrapper
+            className="pannel-menu"
+            callBack={() => setDisplayMenu(false)}
+          >
+            {consts.PANNEL_OPTIONS.map((option, i) => (
+              <p key={i} onClick={() => handleOption(option)}>
+                {option}
+              </p>
+            ))}
+          </DefocusWrapper>
+        )}
       </div>
       <div className="project-pannel-card-container">
         {dummyData.map((d) => (
@@ -41,9 +74,10 @@ const ProjectPannel = ({ name }: Props) => {
           <AddIcon />
           Add New Card
         </p>
-        <MoreHorizRoundedIcon
-          onClick={() => console.log(name)}
+        <DeleteIcon
+          fontSize="small"
           className="initial-icon"
+          onClick={() => console.log("Delete Column")}
         />
       </div>
     </div>
--- client/lib/data.ts
+++ client/lib/data.ts
@@ -1,8 +1,11 @@
 const TEMP_IMG: string =
   "https://media.istockphoto.com/photos/speed-motion-stripe-neon-colorful-abstract-blue-blurred-prism-lines-picture-id1162198296?k=6&m=1162198296&s=612x612&w=0&h=aeExpJF7G-Am291gKP_XeE0TPXG1Vp19fZe8W2KDdm4=";
 
+const PANNEL_OPTIONS = ["Add Card", "Rename Column", "Delete Column"];
+
 const CONSTS = {
   TEMP_IMG,
+  PANNEL_OPTIONS,
 };
 
 export default CONSTS;
