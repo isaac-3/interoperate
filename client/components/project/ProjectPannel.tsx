bebe1f17825000e4419e06b48ae395aa8225c52e
isaac-3 <isaacgc0596@gmail.com>
2021-07-06 14:36:35 -0500

    add scrollbar mixin, add  footer, scrolling to pannel and styling

--- client/components/project/ProjectPannel.tsx
+++ client/components/project/ProjectPannel.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 import ListCard from './ListCard';
+import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
+import AddIcon from '@material-ui/icons/Add';
 
 interface Props {
   name: string;
 }
 
 const ProjectPannel = ({ name }: Props) => {
-  const dummyData = ["List-Item1", "List-Item2", "List-Item3", "List-Item4"];
+
+  const dummyData = [
+    "List-Item1",
+    "List-Item2",
+    "List-Item3",
+    "List-Item4",
+    "List-Item5",
+    "List-Item6",
+    "List-Item7",
+    "List-Item8",
+    "List-Item9",
+    "List-Item10",
+  ];
 
   return (
     <div className="project-pannel">
@@ -18,9 +31,21 @@ const ProjectPannel = ({ name }: Props) => {
           className="initial-icon"
         />
       </div>
-      {dummyData.map((d) => (
-        <ListCard key={d} content={d}/>
-      ))}
+      <div className="project-pannel-card-container">
+        {dummyData.map((d) => (
+          <ListCard key={d} content={d} />
+        ))}
+      </div>
+      <div className="project-pannel-footer">
+        <p>
+          <AddIcon />
+          Add New Card
+        </p>
+        <MoreHorizRoundedIcon
+          onClick={() => console.log(name)}
+          className="initial-icon"
+        />
+      </div>
     </div>
   );
 };
