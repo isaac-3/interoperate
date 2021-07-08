be84bedb43e513286b489f136b1f89f4624380e1
isaac-3 <isaacgc0596@gmail.com>
2021-07-07 20:22:35 -0500

    add new card functionality

--- client/components/project/ProjectPannel.tsx
+++ client/components/project/ProjectPannel.tsx
@@ -1,10 +1,23 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ListCard from './ListCard';
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DefocusWrapper from '../util/DefocusWrapper';
 import consts from '../../lib/data';
+
+const dummyData = [
+  "List-Item1",
+  "List-Item2",
+  "List-Item3",
+  "List-Item4",
+  "List-Item5",
+  "List-Item6",
+  "List-Item7",
+  "List-Item8",
+  "List-Item9",
+  "List-Item10",
+];
 
 interface Props {
   name: string;
@@ -13,30 +26,23 @@ interface Props {
 const ProjectPannel = ({ name }: Props) => {
   const [displayMenu, setDisplayMenu] = useState(false);
   const [pannelTitle, setPannelTitle] = useState(name);
+  const [displayNewCard, setDisplayNewCard] = useState(false);
+  const [pannelItems, setPannelItems] = useState(dummyData);
+  const [newCardData, setNewCardData] = useState("");
   const initialTitle = useRef<string>(name);
-  const titleInput = useRef<HTMLInputElement>(null);
-
-  const dummyData = [
-    "List-Item1",
-    "List-Item2",
-    "List-Item3",
-    "List-Item4",
-    "List-Item5",
-    "List-Item6",
-    "List-Item7",
-    "List-Item8",
-    "List-Item9",
-    "List-Item10",
-  ];
+  const titleInputRed = useRef<HTMLInputElement>(null);
+  const newCardRef = useRef<HTMLTextAreaElement>(null);
+  const pannelContainerRef = useRef<HTMLDivElement>(null);
 
   const handleOption = (option: string) => {
     switch (option) {
       case "Add Card": {
-        console.log(option);
+        setDisplayNewCard(!displayNewCard);
+        setDisplayMenu(false);
         break;
       }
       case "Rename Column": {
-        titleInput?.current?.focus();
+        titleInputRed.current?.focus();
         setDisplayMenu(false);
         break;
       }
@@ -47,29 +53,55 @@ const ProjectPannel = ({ name }: Props) => {
     }
   };
 
-  const handleSave = () => {
-    titleInput?.current?.blur();
-    if (pannelTitle.trim().length === 0) {
-      setPannelTitle(initialTitle.current);
-    } else if (pannelTitle !== initialTitle.current) {
-      initialTitle.current = pannelTitle;
+  const handleSave = (item: string) => {
+    switch (item) {
+      case "new-pannel-name":
+        titleInputRed.current?.blur();
+        if (pannelTitle.trim().length === 0) {
+          setPannelTitle(initialTitle.current);
+        } else if (pannelTitle !== initialTitle.current) {
+          initialTitle.current = pannelTitle;
+        }
+        break;
+      case "new-card":
+        newCardRef.current?.blur();
+        if (newCardData.trim().length === 0) {
+          console.log("no nmae insteted");
+          setDisplayNewCard(false);
+        } else {
+          console.log("good");
+          setDisplayNewCard(false);
+          setPannelItems([...pannelItems, newCardData]);
+          setNewCardData("");
+        }
+        break;
     }
     // TODO: Save new name
   };
+
+  useEffect(() => {
+    if (displayNewCard) {
+      if (pannelContainerRef.current) {
+        pannelContainerRef.current.scrollTop =
+          pannelContainerRef.current.scrollHeight;
+        newCardRef.current?.focus();
+      }
+    }
+  }, [displayNewCard]);
 
   return (
     <div className="project-pannel">
       <div className="project-pannel-title">
         <input
           data-valid={pannelTitle.length !== 0}
-          ref={titleInput}
+          ref={titleInputRed}
           value={pannelTitle}
           onChange={(e) => setPannelTitle(e.target.value)}
-          onBlur={() => handleSave()}
+          onBlur={() => handleSave("new-pannel-name")}
           onKeyDown={(e) => {
             const key = e.keyCode || e.charCode;
             if (key === 13 && e.shiftKey === false) {
-              handleSave();
+              handleSave("new-pannel-name");
             }
           }}
         />
@@ -90,13 +122,28 @@ const ProjectPannel = ({ name }: Props) => {
           </DefocusWrapper>
         )}
       </div>
-      <div className="project-pannel-card-container">
-        {dummyData.map((d) => (
+      <div className="project-pannel-card-container" ref={pannelContainerRef}>
+        {pannelItems.map((d) => (
           <ListCard key={d} content={d} />
         ))}
+        {displayNewCard && (
+          <textarea
+            ref={newCardRef}
+            className="project-new-card"
+            value={newCardData}
+            onChange={(e) => setNewCardData(e.target.value)}
+            onBlur={() => handleSave("new-card")}
+            onKeyDown={(e) => {
+              const key = e.keyCode || e.charCode;
+              if (key === 13 && e.shiftKey === false) {
+                handleSave("new-card");
+              }
+            }}
+          />
+        )}
       </div>
       <div className="project-pannel-footer">
-        <p>
+        <p onClick={() => setDisplayNewCard(!displayNewCard)}>
           <AddIcon />
           Add New Card
         </p>
