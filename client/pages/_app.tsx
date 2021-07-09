0defe2d122c9710b9d4a364b3bd89ade6bfe34b4
isaac-3 <isaacgc0596@gmail.com>
2021-07-09 02:37:14 -0500

    setup client with server

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,45 @@
-import '../styles/styles.scss'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import store from '../store'
-import ModalWrapper from '../components/modal/ModalWrapper'
+import '../styles/styles.scss';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../store';
+import ModalWrapper from '../components/modal/ModalWrapper';
+import {
+  ApolloClient,
+  ApolloProvider,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(message);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:9000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <title>Interoperate</title>
-      <ModalWrapper>
-        <Component {...pageProps} />
-      </ModalWrapper>
-    </Provider>
-  )
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <title>Interoperate</title>
+        <ModalWrapper>
+          <Component {...pageProps} />
+        </ModalWrapper>
+      </Provider>
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default MyApp;
