8a02a82d07b4a5b792a6ab46f487ca5b7db8886b
isaac-3 <isaacgc0596@gmail.com>
2021-10-18 09:25:04 -0500

    extract mongoDB connections to seperate file

--- server/server.js
+++ server/server.js
@@ -1,27 +1,20 @@
-import mongoose from "mongoose";
 import "dotenv/config.js";
 import app from "./app.js";
 import server from "./graphql/index.js";
+import connectDB from "./db/index.js";
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-
-    await mongoose.connection.once("open", () =>
-      console.log(`Connected to mongoDB`)
-    );
-    await app.listen({ port: process.env.PORT }, () =>
+    console.log("Connecting to mongoDB...");
+    await connectDB();
+    console.log("Connected to mongoDB!");
+    app.listen({ port: process.env.PORT }, () =>
       console.log(
         `Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
       )
     );
   } catch {
-    console.log("Not able to run GraphQL server");
+    console.log("Not able to start server...");
   }
 };
 
